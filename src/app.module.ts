631e8a0217abd7314eb6ab0d612e74d67e06da00
fengjutian <>
2023-10-23 14:49:42 +0800

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
+import { LogController } from './log/log.controller';
+import { LogService } from './log/log.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
     CatsModule,
   ],
+  controllers: [LogController],
+  providers: [LogService],
 })
 export class AppModule {}
---
+++ src/log/log.controller.ts
@@ -0,0 +1,79 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { resolve } from 'path';
+import { existsSync, mkdirSync, writeFileSync, appendFileSync, createWriteStream } from 'fs';
+import * as csv from '@fast-csv/format';
+import * as XLSX from 'xlsx';
+
+interface log {
+  id: string;
+  key: number;
+  value: string;
+}
+
+@Controller('log')
+export class LogController {
+  @Get()
+  async getLog() {
+    return 'hello log';
+  }
+
+  @Post()
+  async getUploadLog(@Body() logInfo: log) {
+    const dbFoldName = 'db';
+    const dbFileName = 'db/log.csv';
+    const isExistDB = existsSync(dbFoldName);
+
+    let arrayData = [
+      ['name', 'age'],
+      ['zhangsan', 20],
+      ['lisi', 21],
+      ['wangwu', 22],
+      ['zhaoliu', 23],
+      ['sunqi', 24],
+    ];
+    
+    let jsonData = [{
+      name: "zhangsan1",
+      age: 30
+    }, {
+      name: "lisi1",
+      age: 31
+    }, {
+      name: "wangwu1",
+      age: 32
+    }, {
+      name: "zhaoliu1",
+      age: 33
+    }, {
+      name: "sunqi1",
+      age: 34,
+      length: "hello"
+    }];
+    
+    // 将数据转成workSheet
+    let jsonWorkSheet = XLSX.utils.json_to_sheet([logInfo]);
+    // 构造workBook
+    let workBook = {
+      SheetNames: ['Sheet1'],
+      Sheets: {
+        'Sheet1': jsonWorkSheet,
+      }
+    };
+    // 将workBook写入文件
+    XLSX.writeFile(workBook, "mySheetName.xlsx");
+
+    console.log('到这里。。。。');
+
+    if (isExistDB) {
+      // appendFileSync(dbFileName, 'world');
+    } else {
+      mkdirSync(dbFoldName, { recursive: true });
+
+
+      // writeFileSync(dbFileName, 'hello');
+    }
+
+    console.log('logInfo', logInfo, isExistDB);
+    return logInfo;
+  }
+}
